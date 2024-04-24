e1a55fb4f123a0977122ae2890e122f06b249728
dirFY24KazatoKunii <kazato.kunii.n323@dirbato.co.jp>
2024-04-24 12:50:19 +0900

    「EmployeeValidator、EmployeeService作成」

---
+++ daily_report_system/src/main/java/Services/ServiceBase.java
@@ -0,0 +1,25 @@
+package Services;
+
+import javax.persistence.EntityManager;
+
+import utils.DButil;
+
+/**
+ * DB接続に関わる共通処理を行うクラス
+ */
+public class ServiceBase {
+
+    /**
+     * EntityManagerインスタンス
+     */
+    protected EntityManager em = DButil.createEntityManager();
+
+    /**
+     * EntityManagerのクローズ
+     */
+    public void close() {
+        if (em.isOpen()) {
+            em.close();
+        }
+    }
+}
